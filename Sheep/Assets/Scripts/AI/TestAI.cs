5becbd06769e9bc70bbd70f5182f9d7fab1e75af
mhunek <mhunek@seznam.cz>
2014-10-05 22:00:09 +0200

    Kostička používající navmesh, pro učely testování navmesh

---
+++ Sheep/Assets/Scripts/AI/TestAI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestAI : MonoBehaviour {
+
+     NavMeshAgent agent;
+     public GameObject dest;
+     private GameObject instance;
+
+     int counter = 60;
+	// Use this for initialization
+	void Start () {
+	 agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter--;
+        if (counter == 0)
+        {
+            
+            counter = 150;
+            if (instance != null)
+            {
+                Destroy(instance);
+            }
+            Vector3 destination =new Vector3( Random.Range(0,10)-5,0,Random.Range(0,10)-5);
+            instance = (GameObject)Instantiate(dest, destination, new Quaternion());
+            Debug.Log(destination);
+            //agent.destination = (destination);
+            agent.SetDestination(destination);
+        }
+        //agent.Move
+	}
+}
---
+++ Sheep/Assets/Scripts/AI/TestMoveObstacle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMoveObstacle : MonoBehaviour {
+
+
+    int counter = 90;
+    Vector3 direction = new Vector3(1, 0, 0);
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        counter--;
+        if (counter == 0)
+        {
+            direction = direction * -1;
+            counter = 90;
+        }
+        this.transform.Translate(direction * Time.deltaTime);
+	}
+}

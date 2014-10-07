2a10948293a90b2feb6669a8aff27633cd129750
tiptoe <tiptoecz@gmail.com>
2014-10-08 01:15:52 +0200

    Fence drawing + Prototype scene

---
+++ Sheep/Assets/Scripts/Controls/ChangeFenceColor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeFenceColor : MonoBehaviour
+{
+    public float minLength = 1;
+    public float maxLength = 8;
+    public Color red = Color.red;
+    public Color green = Color.green;
+
+    public bool IsRed { get; private set; }
+    bool inCollision = false;
+    bool isGhostFence = false;
+
+    public void SetGhostFence()
+    {
+        isGhostFence = true;
+        transform.collider.isTrigger = true;
+        transform.GetChild(0).GetComponent<NavMeshObstacle>().enabled = false;
+        SetGreen();
+    }
+
+    void Update()
+    {
+        if (isGhostFence && !inCollision) 
+        {
+            float length = transform.parent.localScale.z;
+            if (length < minLength || length > maxLength)
+                SetRed();
+            else
+                SetGreen();
+        } 
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        // if it is a ghost fence
+        if (isGhostFence)
+        {
+            inCollision = true;
+            SetRed();
+        }
+            
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if (transform.collider.isTrigger)
+        {
+            inCollision = false;
+            SetGreen();
+        }
+            
+    }
+
+    void SetGreen()
+    {
+        transform.renderer.material.color = green;
+        IsRed = false;
+        
+    }
+
+    void SetRed()
+    {
+        transform.renderer.material.color = red;
+        IsRed = true;
+    }
+
+
+}
--- Sheep/Assets/Scripts/Controls/FenceDraw.cs
+++ Sheep/Assets/Scripts/Controls/FenceDraw.cs
@@ -1,71 +1,71 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class FenceDraw : MonoBehaviour
 {
-    //bude nahrazeno prefabem plotu
     public GameObject fencePrefab;
-    /// <summary>
-    /// pamatuje si poslední vytvořený plot, už kvuli tomu že ho bude mazat
-    /// </summary>
-    List<GameObject> fence = new List<GameObject>();
 
-    //kde začíná plot
-    private Vector3 startDrawPosition;
-    private bool draw = false;
+    Vector3 startPosition;
+    Vector3 endPosition;
 
-	// Use this for initialization
-	void Start () {
-       
-	}
-	
-	// Update is called once per frame
-	void Update () {
-    #if UNITY_EDITOR
+    GameObject fenceSolid;
+    GameObject fenceGhost;
+    ChangeFenceColor fenceGhostScript;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+	void Update () 
+    {
         if (Input.GetMouseButton(0))
         {
-            if (!draw)
+            RaycastHit hit;
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(mouseRay, out hit, 100))
             {
-                Debug.Log("start");
-                foreach (GameObject gO in fence)
-                {
-                    Destroy(gO);
-                }
-                RaycastHit data;
-                Ray vRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                if (Physics.Raycast(vRay, out data, 1000))
-                {
-                    Debug.Log("data recieve");
-                    draw = true;
-                    startDrawPosition = new Vector3(data.point.x,0, data.point.z);
-                }
-            }
-        }
-        else
-        {
-            if (draw)
-            {
-                Debug.Log("end");
-                draw = false;
-                RaycastHit data;
-                Ray vRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-                //podmínka prakticky zbytečná protože nepujde minout plochu
-                if (Physics.Raycast(vRay, out data, 1000))
-                {
-                    Debug.Log("placing");
-                    Vector3 endDrawPosition = new Vector3(data.point.x,0, data.point.z);
-                    float distance = Vector3.Distance(startDrawPosition, endDrawPosition);
-                    // pak místo toho se zde bude počítat uhel, kontrolovat kolize a řešit rozměry plotu
-                    for (int i = 0; i < 10; i++)
-                    {
-                      fence.Add((GameObject)Instantiate(fencePrefab, startDrawPosition, new Quaternion()));
-                       startDrawPosition = Vector3.MoveTowards(startDrawPosition, endDrawPosition, distance / 10.0f);
-                    }
-                }
-            }
+                if (Input.GetMouseButtonDown(0))
+                    startPosition = new Vector3(hit.point.x, 0, hit.point.z);
 
+                endPosition = new Vector3(hit.point.x, 0, hit.point.z);
+                UpdatePosition(fenceGhost, startPosition, endPosition);
+            }  
         }
-    #endif
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            HideFence(fenceGhost);
+
+            if (!fenceGhostScript.IsRed)
+                UpdatePosition(fenceSolid, startPosition, endPosition);    
+        }
 	}
+
+    void UpdatePosition(GameObject fence, Vector3 startPosition, Vector3 endPosition)
+    {
+        Debug.Log("start:" + startPosition);
+        Debug.Log("end:" + endPosition);
+        fence.transform.position = startPosition;
+        fence.transform.LookAt(endPosition);
+        fence.transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y, Vector3.Distance(startPosition, endPosition));
+    }
+
+    void Initialize()
+    {
+        fenceSolid= (GameObject)Instantiate(fencePrefab);
+        HideFence(fenceSolid);
+        Debug.Log("Solid fence created.");
+
+        fenceGhost = (GameObject)Instantiate(fencePrefab);
+        fenceGhostScript = fenceGhost.transform.GetChild(0).GetComponent<ChangeFenceColor>();
+        fenceGhostScript.SetGhostFence();
+        HideFence(fenceGhost);
+        Debug.Log("Ghost fence created.");
+    }
+
+    void HideFence(GameObject fence)
+    {
+        fence.transform.position = new Vector3(100, 100, 100);
+    }
 }

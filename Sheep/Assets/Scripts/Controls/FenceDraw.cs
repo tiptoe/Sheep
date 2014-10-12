98bf4267a0e3d04228b3b759c7d7d700a4ac9933
mhunek <mhunek@seznam.cz>
2014-10-12 20:10:37 +0200

    upravena kolize pro plot, místo jedné velké překážky bude bude více malých, umožní to líp řešit když se plot bude natahovat/zkracovat, trochu více výkonu (ale problémy dělat nebude) a mnohem větší přesnost

--- Sheep/Assets/Scripts/Controls/FenceDraw.cs
+++ Sheep/Assets/Scripts/Controls/FenceDraw.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FenceDraw : MonoBehaviour
 {
@@ -7,6 +8,9 @@ public class FenceDraw : MonoBehaviour
 
     Vector3 startPosition;
     Vector3 endPosition;
+
+    public GameObject fenceObstacle;
+    List<GameObject> obstacles = new List<GameObject>();
 
     GameObject fenceSolid;
     GameObject fenceGhost;
@@ -38,7 +42,11 @@ public class FenceDraw : MonoBehaviour
             HideFence(fenceGhost);
 
             if (!fenceGhostScript.IsRed)
-                UpdatePosition(fenceSolid, startPosition, endPosition);    
+            {
+                UpdatePosition(fenceSolid, startPosition, endPosition);
+                CreatePrecisionObstacle();
+            }
+ 
         }
 	}
 
@@ -67,5 +75,35 @@ public class FenceDraw : MonoBehaviour
     void HideFence(GameObject fence)
     {
         fence.transform.position = new Vector3(100, 100, 100);
+    }
+
+    /// <summary>
+    /// místo jedné velké překážky vytvoří více malých
+    /// je to trochu pomalejší, ale rozhodně přesnější
+    /// volá se vždy když je dokreslen a umístěn plot
+    /// </summary>
+    void CreatePrecisionObstacle()
+    {
+        //smažu předchozí překážky
+        foreach (GameObject gO in obstacles)
+        {
+            Destroy(gO);
+        }
+        //vzdálenost, počet malých překážek které je potřeba umístit
+        float dinstance = Vector3.Distance(startPosition, endPosition);
+        float smallerObstaclesCount = dinstance / (0.5f);
+        Vector3 startObstaclesPosition = startPosition;
+        //první bude vždy na začátku
+        obstacles.Add((GameObject)GameObject.Instantiate(fenceObstacle, startObstaclesPosition, new Quaternion()));
+        startObstaclesPosition = Vector3.MoveTowards(startObstaclesPosition, endPosition, 0.5f);
+        for (int i = 0; i < smallerObstaclesCount-1; i++)
+        {
+           //několik mezitim
+            obstacles.Add((GameObject)GameObject.Instantiate(fenceObstacle, startObstaclesPosition, new Quaternion()));
+           startObstaclesPosition = Vector3.MoveTowards(startObstaclesPosition, endPosition, 0.5f);
+        }
+        //poslední bude vždy na konci
+        obstacles.Add((GameObject)GameObject.Instantiate(fenceObstacle, endPosition, new Quaternion()));
+        startObstaclesPosition = Vector3.MoveTowards(startObstaclesPosition, endPosition, 0.5f);
     }
 }

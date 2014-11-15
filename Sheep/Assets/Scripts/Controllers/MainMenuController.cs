7e089e1bc6110b3cd2abdf9debd199c82b84e738
Tomáš Pouzar <tomas.pouzar@gmail.com>
2014-11-16 00:39:52 +0100

    Vertical slice: nova scena s provizornim hlavnim menu

---
+++ Sheep/Assets/Scripts/Controllers/MainMenuController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+	public RectTransform Start;
+	public RectTransform Play;
+	public RectTransform Options;
+	public RectTransform About;
+
+	public void ChangeToStart(RectTransform activePage)
+	{
+		activePage.gameObject.SetActive(false);
+		Start.gameObject.SetActive(true);
+	}
+
+	public void ChangeToPlay(RectTransform activePage)
+	{
+		Application.LoadLevel("VerticalSlice");
+		//activePage.gameObject.SetActive(false);
+		//Play.gameObject.SetActive(true);
+	}
+
+	public void ChangeToOptions(RectTransform activePage)
+	{
+		activePage.gameObject.SetActive(false);
+		Options.gameObject.SetActive(true);
+	}
+
+	public void ChangeToAbout(RectTransform activePage)
+	{
+		activePage.gameObject.SetActive(false);
+		About.gameObject.SetActive(true);
+	}
+}
---
+++ Sheep/Assets/Scripts/ObjectsBehaviour/LagoonBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LagoonBehaviour : MonoBehaviour {
+
+	public LevelController _LevelController;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.gameObject.tag == "Sheep")
+		{
+			collision.gameObject.GetComponent<SheepAI>().SetDead();
+			collision.gameObject.SetActive(false);
+			_LevelController.AnimalDied(Animals.Sheep);
+		}
+	}
+}

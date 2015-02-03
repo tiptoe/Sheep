a687affbadf5e8706b8078be14e1ce0006e6b927
tiptoe <tiptoecz@gmail.com>
2015-02-03 16:13:25 +0100

    Tools trigger zones

---
+++ Sheep/Assets/Scripts/Tools/Support/MeatPrefab.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeatPrefab : MonoBehaviour, ITriggerZoneOwner
+{
+
+    void Update()
+    {
+    }
+
+    public void OnTriggerZoneEnter(Collider other, TriggerZone triggerZone)
+    {
+        // outer trigger zone
+        if (triggerZone.id == 1)
+        {
+        }
+
+        // inner trigger zone
+        else if (triggerZone.id == 0)
+        {
+        }
+    }
+
+    public void OnTriggerZoneExit(Collider other, TriggerZone triggerZone)
+    {
+    }
+}
